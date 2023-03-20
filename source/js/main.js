74f1f7b063eaf349b4e1ea9df19b4f029f59a09d
Sweezar <royal13flush@gmail.com>
2023-03-20 16:18:38 +0300

    Сделал карту с местоположением адаптивной для разных разрешений -картинку на главной странице сделал адаптивной

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,6 @@ let navList = document.querySelector(".main-nav__list");
 navList.classList.remove("main-nav__list--no-js");
 navList.classList.add("main-nav__list--closed");
 
-navToggler.addEventListener('click', ()=> {
+navToggler.addEventListener("click", () => {
   navList.classList.toggle("main-nav__list--closed");
-})
+});
